def52427db32c29f972aada6120d68ea89732ef4
Madhur Ahuja <madhur@users.noreply.github.com>
2022-11-12 13:23:40 +0530

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 source ~/pyenv3/bin/activate
-/home/madhur/github/personal/i3scripts/autoname_workspaces.py >> /home/madhur/logs/output.log  2>&1
+~/github/personal/i3scripts/autoname_workspaces.py >> ~/logs/output.log  2>&1
